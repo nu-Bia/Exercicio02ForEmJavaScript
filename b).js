81f675d36b399fc00944ac5b02dbd41c4c735989
Núbia <112019351+nu-Bia@users.noreply.github.com>
2022-11-04 13:27:11 -0300

    Add files via upload

---
+++ b).js
@@ -0,0 +1,7 @@
+function tabuada() {
+     var numero = parseInt(prompt("Digite um número: "));
+     for (var contadora = 1; contadora <= 10; contadora++) {
+         document.write(numero + " X " + contadora + " = " + contadora * numero + "</br>");
+     }
+ };
+    
